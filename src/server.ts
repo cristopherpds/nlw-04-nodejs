92d3fe4cfe9820981eed6543f65d35ef1058b888
Cristopher Paiva <josuepaiva7@gmail.com>
2021-02-23 11:29:22 -0300

    🚀

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
 import "./database";
+import { router } from './routes';
 
 const app = express();
+app.use(express.json());
+app.use(router);
 
 
-
-app.get('/', (req, res) => {
-    return res.json({message:'hello word'})
-})
-
-
-app.post('/', (req, res) => {
-    return res.json({message:'dados salvos com sucesso!'})
-})
 
 app.listen(3333, ()=> console.log('server run'));
